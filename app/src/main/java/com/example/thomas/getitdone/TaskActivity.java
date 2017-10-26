ff5ea66bd3fabf3351efa3cbe5578c19664c4b9e
Get It Done 2017 <getitdonebots@gmail.com>
2017-10-26 13:38:00 +0400

    First commit

---
+++ app/src/main/java/com/example/thomas/getitdone/TaskActivity.java
@@ -0,0 +1,19 @@
+package com.example.thomas.getitdone;
+
+/**
+ * Created by All on 10/26/2017.
+ */
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks);
+    }
+
+}
