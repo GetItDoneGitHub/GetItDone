6b10f460b1b0390402455a2ab3c2355fc5ea6ddf
Get It Done 2017 <getitdonebots@gmail.com>
2017-10-26 13:22:15 +0400

    First commit

---
+++ app/src/main/java/com/example/thomas/getitdone/CreditCardActivity.java
@@ -0,0 +1,14 @@
+package com.example.thomas.getitdone;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CreditCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creditcard);
+    }
+
+}
